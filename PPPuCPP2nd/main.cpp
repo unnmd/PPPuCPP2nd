c7e0d2e4678b7841fb6418c21a0b7986783f1c61
Vitalii Ovchinnikov <vit.ovchinnikov@gmail.com>
2023-07-21 13:43:27 +0300

    init

---
+++ PPPuCPP2nd/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PPPuCPP2nd
+//
+//  Created by Vitaly Ovchinnikov on 21.07.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
