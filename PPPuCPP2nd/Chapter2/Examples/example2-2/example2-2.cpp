e94d366b0377f0bb7bd822c3aa2198e111f3847b
Vitalii Ovchinnikov <vit.ovchinnikov@gmail.com>
2023-07-21 16:37:57 +0300

    Изменил структуру

--- PPPuCPP2nd/Chapter2/Examples/example2-2/example2-2.cpp
+++ PPPuCPP2nd/Chapter2/Examples/example2-2/example2-2.cpp
@@ -1,15 +1,16 @@
 //
 //  main.cpp
-//  exercise1
+//  example2-2
 //
 //  Created by Vitaly Ovchinnikov on 21.07.2023.
 //
 
-#include "../../../../lib/std_lib_facilities.h"
+#include "../../../lib/std_lib_facilities.h"
 
 int main()
   {
-    cout << "Hello, world!\nHere we go!\n";
+    cout << "Hello, world!\n";
     //keep_window_open();
     return 0;
+
   }
--- PPPuCPP2nd/Chapter2/Exercises/exercise1/exercise1.cpp
+++ PPPuCPP2nd/Chapter2/Exercises/exercise1/exercise1.cpp
@@ -1,16 +1,15 @@
 //
 //  main.cpp
-//  example2-2
+//  exercise1
 //
 //  Created by Vitaly Ovchinnikov on 21.07.2023.
 //
 
-#include "../../../../lib/std_lib_facilities.h"
+#include "../../../lib/std_lib_facilities.h"
 
 int main()
   {
-    cout << "Hello, world!\n";
+    cout << "Hello, world!\nHere we go!\n";
     //keep_window_open();
     return 0;
-
   }
