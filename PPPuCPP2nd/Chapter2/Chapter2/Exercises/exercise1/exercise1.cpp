558c58a57f3ab78e1173911215a43fca451b8d4b
Vitalii Ovchinnikov <vit.ovchinnikov@gmail.com>
2023-07-21 16:31:00 +0300

    Добавил пример и упражнение по 2й главе

---
+++ PPPuCPP2nd/Chapter2/Chapter2/Exercises/exercise1/exercise1.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  exercise1
+//
+//  Created by Vitaly Ovchinnikov on 21.07.2023.
+//
+
+#include "../../../../lib/std_lib_facilities.h"
+
+int main()
+  {
+    cout << "Hello, world!\nHere we go!\n";
+    //keep_window_open();
+    return 0;
+  }
---
+++ PPPuCPP2nd/Chapter2/Chapter2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Chapter2
+//
+//  Created by Vitaly Ovchinnikov on 21.07.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
